e7e03352fabcfbe00ae1bc14861835ff2f223ead
Reza Akhlaghi <110752990+iRezaAkhlaghi@users.noreply.github.com>
2025-07-24 18:24:08 +0330

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { Toaster } from "react-hot-toast";
 
 import { BrowserRouter, Routes, Route } from "react-router-dom";
@@ -10,8 +9,7 @@ const queryClient = new QueryClient();
 function App() {
   return (
     <QueryClientProvider client={queryClient}>
-      <ReactQueryDevtools initialIsOpen={false} />
-   
+    
         <BrowserRouter>
           <Routes>
             <Route path="/:cityName?" element={<Home />} />
